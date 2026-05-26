494fc964ee0ca49a026c1822ad83be1d48b1a0a2
Oscar Faldi <oscarfaldi@gmail.com>
2026-05-26 13:05:35 +0700

    Update and rename network.nix to nic.nix

---
+++ nic.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+  # Intel X540-T2 10G direct NAS connection
+  networking.interfaces.enp4s0f0.ipv4.addresses = [
+    {
+      address = "10.10.20.11";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.interfaces.enp4s0f1.ipv4.addresses = [
+    {
+      address = "10.10.20.21";
+      prefixLength = 24;
+    }
+  ];
+}
