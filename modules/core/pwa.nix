1b177372b68d2fb4342a530467722032eaa989cc
Oscar Faldi <oscarfaldi@gmail.com>
2026-05-30 21:06:37 +0700

    Update pwa.nix

--- modules/core/pwa.nix
+++ modules/core/pwa.nix
@@ -39,6 +39,12 @@ in
     })
 
     (mkPWA {
+      name = "Gemini";
+      url = "https://gemini.google.com";
+      icon = "/home/oscarfaldi/.system/assets/icons/gemini.png";
+    })
+
+    (mkPWA {
       name = "WhatsApp";
       url = "https://web.whatsapp.com";
       icon = "/home/oscarfaldi/.system/assets/icons/whatsapp.svg";
