01abdecb262d7f34781c043cad0eae6e554a2501
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-01 21:36:13 +0700

    Create theme.sh

---
+++ theme.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme Graphite-Dark
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Classic
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
