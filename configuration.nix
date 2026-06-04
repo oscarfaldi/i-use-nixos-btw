6babea8ccd45c49ed5a52da2366b440ae6ba3615
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-05 00:25:29 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -68,14 +68,11 @@
 
   # Enable low-latency audio scheduling
   security.rtkit.enable = true;
-  
-  # Enable Pulseaudio
+
+  # Enable pulseaudio stack
   hardware.pulseaudio = {
     enable = true;
-
-    # NixOS allows either a lightweight build (default) or full build of PulseAudio to be installed.
-    # Only the full build has Bluetooth support, so it must be selected here.
-    package = pkgs.pulseaudioFull;
+    support32Bit = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
