8ce25f5042a37966259c1944aa7a1f2e32efc975
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-06 23:15:24 +0700

    Add theme scripts for graphite and vintage

--- modules/desktop/desktop-assets.nix
+++ modules/desktop/desktop-assets.nix
@@ -1,17 +1,27 @@
 { pkgs, ... }:
 
 let
+
   mkScript = name:
     pkgs.writeShellScriptBin name
       (builtins.readFile ../../assets/scripts/${name});
 
+  mkTheme = name: path:
+    pkgs.writeShellScriptBin name
+      (builtins.readFile path);
+
 in
 {
   environment.systemPackages = [
+
     (mkScript "theme-toggle")
-    (mkScript "theme-graphite")
-    (mkScript "theme-vintage")
     (mkScript "wallpaper")
     (mkScript "wallpaperd")
+
+    (mkTheme "theme-graphite"
+      ../../assets/themes/graphite/apply)
+
+    (mkTheme "theme-vintage"
+      ../../assets/themes/vintage/apply)
   ];
 }
