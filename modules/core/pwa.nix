4ec56addfcae508f6b10c30e73ea624e6b959c3b
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-07 16:59:02 +0700

    Update pwa.nix

--- modules/core/pwa.nix
+++ modules/core/pwa.nix
@@ -69,6 +69,12 @@ in
     })
 
     (mkPWA {
+      name = "Jellyfin";
+      url = "http://10.10.10.10:8096/";
+      icon = "/etc/nixos/assets/icons/jellyfin.svg";
+    })
+
+    (mkPWA {
       name = "Navidrome";
       url = "http://10.10.10.10:4533/";
       icon = "/etc/nixos/assets/icons/navidrome.svg";
