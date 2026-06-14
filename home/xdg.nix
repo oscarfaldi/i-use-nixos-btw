1660fad757bdc33f8771de7f8d96654750f3d064
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-14 15:09:35 +0700

    Update xdg.nix

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,4 +1,20 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
+  xdg = {
+    enable = true;
+
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+
+      desktop = "$HOME";
+      documents = "$HOME/Documents";
+      download = "$HOME/Downloads";
+      music = "$HOME/Music";
+      pictures = "$HOME/Pictures";
+      videos = "$HOME/Videos";
+      templates = "$HOME/Templates";
+    };
+  };
 }
