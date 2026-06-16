6a262d54659fc8ad7dd27c7a629768f71477f79f
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-16 23:39:30 +0700

    Update xdg.nix

--- home/xdg.nix
+++ home/xdg.nix
@@ -19,29 +19,6 @@
     };
 
     mime.enable = true;
-
-    portal = {
-      enable = true;
-
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-gnome
-      ];
-
-      config.niri = {
-        preferred = {
-          "org.freedesktop.impl.portal.FileChooser" = "gtk";
-          "org.freedesktop.impl.portal.AppChooser" = "gtk";
-          "org.freedesktop.impl.portal.Notification" = "gtk";
-          "org.freedesktop.impl.portal.Inhibit" = "gtk";
-
-          "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-          "org.freedesktop.impl.portal.Screenshot" = "gnome";
-
-          default = "gtk";
-        };
-      };
-    };
   };
 
   # Activation script to ensure extra directories exist
