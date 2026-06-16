6bff9ade5fafd22927c0cba6843fcce91602867f
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-16 20:44:55 +0700

    Update xdg.nix

--- home/xdg.nix
+++ home/xdg.nix
@@ -4,11 +4,13 @@
   xdg = {
     enable = true;
 
+    # XDG Base Directory User Folders configuration
     userDirs = {
       enable = true;
       createDirectories = true;
       setSessionVariables = true;
 
+      # Standard XDG directories
       desktop = "$HOME/Desktop";
       documents = "$HOME/Documents";
       download = "$HOME/Downloads";
@@ -16,37 +18,37 @@
       pictures = "$HOME/Pictures";
       videos = "$HOME/Videos";
       templates = null;
+
+      # Custom non-standard XDG directories managed natively by Home Manager
+      extraUserDirs = {
+        XDG_GAMES_DIR = "$HOME/Games";
+      };
     };
 
+    # Enable native shared MIME-info management
     mime.enable = true;
 
     # Declarative configuration for Niri desktop portals
     portal = {
       enable = true;
       
-      # Generates ~/.config/xdg-desktop-portal/niri-portals.conf
+      # Generates ~/.config/xdg-desktop-portal/niri-portals.conf automatically
       config.niri = {
         preferred = {
-          # GTK everywhere possible
+          # GTK backend integrations where possible
           "org.freedesktop.impl.portal.FileChooser" = "gtk";
           "org.freedesktop.impl.portal.AppChooser" = "gtk";
           "org.freedesktop.impl.portal.Notification" = "gtk";
           "org.freedesktop.impl.portal.Inhibit" = "gtk";
 
-          # Screencasting configuration
+          # GNOME backend integrations required for display recording
           "org.freedesktop.impl.portal.ScreenCast" = "gnome";
           "org.freedesktop.impl.portal.Screenshot" = "gnome";
 
-          # Global fallback
+          # Global fallback routing
           "default" = "gtk";
         };
       };
     };
   };
-
-  # Activation script to ensure extra directories exist
-  home.activation.createGamesDir =
-    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p "$HOME/Games"
-    '';
 }
