b74baa492d07b32972f117422974edb6e72fe697
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-16 20:42:29 +0700

    Update xdg.nix

--- home/xdg.nix
+++ home/xdg.nix
@@ -19,8 +19,32 @@
     };
 
     mime.enable = true;
+
+    # Declarative configuration for Niri desktop portals
+    portal = {
+      enable = true;
+      
+      # Generates ~/.config/xdg-desktop-portal/niri-portals.conf
+      config.niri = {
+        preferred = {
+          # GTK everywhere possible
+          "org.freedesktop.impl.portal.FileChooser" = "gtk";
+          "org.freedesktop.impl.portal.AppChooser" = "gtk";
+          "org.freedesktop.impl.portal.Notification" = "gtk";
+          "org.freedesktop.impl.portal.Inhibit" = "gtk";
+
+          # Screencasting configuration
+          "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+          "org.freedesktop.impl.portal.Screenshot" = "gnome";
+
+          # Global fallback
+          "default" = "gtk";
+        };
+      };
+    };
   };
 
+  # Activation script to ensure extra directories exist
   home.activation.createGamesDir =
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       mkdir -p "$HOME/Games"
