2fc646cba788b5bffe5703c0f392aa443e8b88b1
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-18 00:36:56 +0700

    Create theme-nordic.nix

---
+++ home/theme-nordic.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+
+      gtk-theme = "Nordic";
+
+      icon-theme = "Papirus-Dark";
+
+      cursor-theme = "Bibata-Modern-Ice";
+    };
+  };
+}
