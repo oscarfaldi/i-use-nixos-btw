7014e03cdc9b1741fee73439244118b589a04e0a
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-18 00:40:08 +0700

    Create theme-graphite.nix

---
+++ home/theme-graphite.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Graphite-Dark";
+      icon-theme = "Papirus-Dark";
+      cursor-theme = "Bibata-Modern-Ice";
+    };
+  };
+}
