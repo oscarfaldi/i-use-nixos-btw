c239f436a765ebccaba67d4b2ad4c2c78e720b1d
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-18 21:54:24 +0700

    Create actions-tools.nix

---
+++ modules/scripts/actions-tools.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+let
+
+  mkScript = name:
+    pkgs.writeShellScriptBin name
+      (builtins.readFile ../../modules/scripts/actions/${name});
+
+in
+{
+
+  # ============================================================
+  # Environment Scripts (Actionable Scripts)
+  # ============================================================
+
+  environment.systemPackages = [
+    (mkScript "screenshot")
+    (mkScript "remux-mkv")
+  ];
+}
