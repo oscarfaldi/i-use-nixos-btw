9c05292ba78f636671ce5119719fb59d32b4d8ee
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-21 02:51:55 +0700

    Update actions-tool.nix

--- modules/scripts/actions-tool.nix
+++ modules/scripts/actions-tool.nix
@@ -16,5 +16,14 @@ in
   environment.systemPackages = [
     (mkScript "screenshot")
     (mkScript "rename-ts-to-mts")
+    (mkScript "convert-to-avif")
+    (mkScript "convert-to-pdf")
+    (mkScript "copy-filename")
+    (mkScript "copy-filename-no-extension")
+    (mkScript "copy-full-path")
+    (mkScript "rotate-image-left")
+    (mkScript "rotate-image-right")
+    (mkScript "rotate-video-left")
+    (mkScript "rotate-video-right")
   ];
 }
