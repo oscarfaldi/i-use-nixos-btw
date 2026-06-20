020f4ca1b43397527ae905fce1ade62071e07d09
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-21 00:34:05 +0700

    Update and rename launchers-tools.nix to launchers-tool.nix

---
+++ modules/scripts/launchers-tool.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+let
+
+  mkScript = name:
+    pkgs.writeShellScriptBin name
+      (builtins.readFile ../../modules/scripts/launchers/${name});
+
+in
+{
+
+  # ============================================================
+  # Environment Scripts (Launchable Tools)
+  # ============================================================
+
+  environment.systemPackages = [
+    (mkScript "config-menu")
+    (mkScript "configuration-menu")
+    (mkScript "modules-menu")
+    (mkScript "repo-menu")
+    (mkScript "scripts-menu")
+  ];
+}
