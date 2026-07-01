869829c01d6f9fe0b3932ba7aebd85eb17d11eba
Oscar Faldi <oscarfaldi@gmail.com>
2026-07-02 00:44:50 +0700

    Update files.nix

--- home/files.nix
+++ home/files.nix
@@ -19,43 +19,44 @@
     # --------------------------------------------------------------------------
     # Core Shell & Terminal Environment
     # --------------------------------------------------------------------------
-    ".config/zsh/.zshrc".source       = ../.config/zsh/.zshrc;
-    ".config/aliasrc".source          = ../.config/aliasrc;
-    ".config/starship.toml".source    = ../.config/starship.toml;
-    ".config/fastfetch".source        = ../.config/fastfetch;
-    ".config/ghostty".source          = ../.config/ghostty;
+    ".config/zsh/.zshrc".source             = ../.config/zsh/.zshrc;
+    ".config/aliasrc".source                = ../.config/aliasrc;
+    ".config/starship.toml".source          = ../.config/starship.toml;
+    ".config/fastfetch".source              = ../.config/fastfetch;
+    ".config/ghostty".source                = ../.config/ghostty;
 
     # --------------------------------------------------------------------------
     # System Interface & Wayland Compositor (Niri)
     # --------------------------------------------------------------------------
-    ".config/niri".source             = ../.config/niri;
-    ".config/waybar".source           = ../.config/waybar;
-    ".config/fuzzel".source           = ../.config/fuzzel;
-    ".config/mako".source             = ../.config/mako;
-    ".config/wlogout".source          = ../.config/wlogout;
-    ".config/assets".source           = ../.config/assets;
+    ".config/niri".source                   = ../.config/niri;
+    ".config/waybar".source                 = ../.config/waybar;
+    ".config/fuzzel".source                 = ../.config/fuzzel;
+    ".config/mako".source                   = ../.config/mako;
+    ".config/wlogout".source                = ../.config/wlogout;
+    ".config/assets".source                 = ../.config/assets;
 
     # --------------------------------------------------------------------------
     # Applications, Viewers & Media Tools
     # --------------------------------------------------------------------------
-    ".config/yazi".source             = ../.config/yazi;
-    ".config/mpv".source              = ../.config/mpv;
-    ".config/qimgv".source            = ../.config/qimgv;
-    ".config/pdfarranger".source      = ../.config/pdfarranger;
-    ".config/xarchiver".source        = ../.config/xarchiver;
+    ".config/yazi".source                   = ../.config/yazi;
+    ".config/mpv".source                    = ../.config/mpv;
+    ".config/qimgv".source                  = ../.config/qimgv;
+    ".config/pdfarranger".source            = ../.config/pdfarranger;
+    ".config/xarchiver".source              = ../.config/xarchiver;
+    ".config/obsidian/obsidian.json".source = ../.config/obsidian/obsidian.json;
 
     # --------------------------------------------------------------------------
     # Desktop Automation, Security & Utilities
     # --------------------------------------------------------------------------
-    ".config/swaylock".source          = ../.config/swaylock;
-    ".config/swayidle".source          = ../.config/swayidle;
-    ".config/Thunar".source            = ../.config/Thunar;
-    ".config/gtk-3.0/bookmarks".source = ../.config/gtk-3.0/bookmarks;
-    ".config/mimeapps.list".source     = ../.config/mimeapps.list;
+    ".config/swaylock".source               = ../.config/swaylock;
+    ".config/swayidle".source               = ../.config/swayidle;
+    ".config/Thunar".source                 = ../.config/Thunar;
+    ".config/gtk-3.0/bookmarks".source      = ../.config/gtk-3.0/bookmarks;
+    ".config/mimeapps.list".source          = ../.config/mimeapps.list;
     
     # System Integration
-    ".config/xdg-desktop-portal".source = ../.config/xdg-desktop-portal;
-    ".local/share/applications".source  = ../.local/share/applications;
+    ".config/xdg-desktop-portal".source     = ../.config/xdg-desktop-portal;
+    ".local/share/applications".source      = ../.local/share/applications;
 
   };
 }
