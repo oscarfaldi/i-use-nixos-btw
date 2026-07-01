c8c7b367f027e52d18f167d055525388afce57a7
Oscar Faldi <oscarfaldi@gmail.com>
2026-07-02 02:10:26 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,19 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Configure Glacier as the primary Nix binary cache.
+  nix.settings = {
+    substituters = [
+      "http://10.10.10.171:8501"
+      "https://cache.nixos.org"
+    ];
+
+    trusted-public-keys = [
+      "ncps.local:MIxzb9CCWYX8qk2Ec79fKD/d4nvc0anIoEL+A7YJVDE="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
