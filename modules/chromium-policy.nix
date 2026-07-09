429db9dbab53748e11a65ae7a40e273b98101e68
Oscar Faldi <oscarfaldi@gmail.com>
2026-07-09 15:43:17 +0700

    Add files via upload

---
+++ modules/chromium-policy.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  environment.etc."chromium/policies/managed/policies.json".text = ''
+    {
+      "PasswordManagerEnabled": false,
+      "AutofillAddressEnabled": false,
+      "AutofillCreditCardEnabled": false,
+
+      "DnsOverHttpsMode": "secure",
+      "DnsOverHttpsTemplates": "https://cloudflare-dns.com/dns-query",
+
+      "BackgroundModeEnabled": false,
+      "ShoppingListEnabled": false,
+
+      "MetricsReportingEnabled": false
+    }
+  '';
+}
