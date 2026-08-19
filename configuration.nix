26482deea780a51ad26d89513fd1d3384bd51ed1
Oscar Faldi <oscarfaldi@gmail.com>
2026-08-19 23:05:21 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,6 @@
       ./modules/chromium-policy.nix
       ./modules/pwa-chromium.nix
       ./modules/games.nix
-      ./modules/smb.nix
       ./modules/content-creation.nix
       ./modules/syncthing.nix
 
@@ -88,9 +87,6 @@
     trusted-public-keys = [
       "ncps.local:MIxzb9CCWYX8qk2Ec79fKD/d4nvc0anIoEL+A7YJVDE="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-
-    connect-timeout = 2;
-    stalled-download-timeout = 5;
     ];
   };
 
