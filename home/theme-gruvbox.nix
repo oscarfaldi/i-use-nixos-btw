5de20530301d9f656dc03af41817d6779102b7a8
Oscar Faldi <oscarfaldi@gmail.com>
2026-09-02 20:30:39 +0700

    Create theme-gruvbox.nix

---
+++ home/theme-gruvbox.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  # ============================================================
+  # Gruvbox GTK Theme
+  # ============================================================
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.gruvbox-dark-gtk;
+      name = "gruvbox-dark";
+    };
+
+    gtk4.theme = config.gtk.theme;
+
+    iconTheme = {
+      package = pkgs.gruvbox-dark-icons-gtk;
+      name = "oomox-gruvbox-dark";
+    };
+
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
+
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 11;
+    };
+
+    gtk3.extraConfig = {
+      gtk-menu-images = true;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+}
