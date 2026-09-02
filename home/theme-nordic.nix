afb76f742e5849d366bf9f580f9aa6cacfcbcf85
Oscar Faldi <oscarfaldi@gmail.com>
2026-09-02 22:49:28 +0700

    Update theme-nordic.nix

--- home/theme-nordic.nix
+++ home/theme-nordic.nix
@@ -55,13 +55,17 @@
   # Fuzzel
   # ============================================================
 
-  programs.fuzzel.settings.colors = {
-    background = "242933cc";
-    text = "bbc3d4ff";
-    match = "5e81acff";
-    selection = "1b1f26ff";
-    selection-text = "bbc3d4ff";
-    selection-match = "5e81acff";
-    border = "5e81ac66";
+  programs.fuzzel.settings = {
+    main.icon-theme = "papirus-icon-theme";
+
+    colors = {
+      background = "242933cc";
+      text = "bbc3d4ff";
+      match = "5e81acff";
+      selection = "1b1f26ff";
+      selection-text = "bbc3d4ff";
+      selection-match = "5e81acff";
+      border = "5e81ac66";
+    };
   };
 }
