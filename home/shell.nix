dd5538a648e80c667dadf71a0ea1983ad500490d
Oscar Faldi <oscarfaldi@gmail.com>
2026-09-02 23:15:35 +0700

    Update shell.nix

--- home/shell.nix
+++ home/shell.nix
@@ -1,10 +1,26 @@
 { config, pkgs, ... }:
 
 {
+  # ============================================================
+  # Zsh
+  # ============================================================
+
   programs.zsh = {
     enable = true;
     dotDir = "${config.xdg.configHome}/zsh";
+
+    initContent = ''
+      # Aliases
+      [ -f "$HOME/.config/aliasrc" ] && source "$HOME/.config/aliasrc"
+
+      # Smart directory jumping
+      command -v zoxide >/dev/null && eval "$(zoxide init zsh)"
+    '';
   };
+
+  # ============================================================
+  # Starship
+  # ============================================================
 
   programs.starship.enable = true;
 }
