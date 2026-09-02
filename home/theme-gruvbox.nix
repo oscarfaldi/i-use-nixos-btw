458f522520db273d4a573bae1a68a6eba1b2e1ef
Oscar Faldi <oscarfaldi@gmail.com>
2026-09-03 00:46:55 +0700

    Update theme-gruvbox.nix

--- home/theme-gruvbox.nix
+++ home/theme-gruvbox.nix
@@ -46,10 +46,26 @@
   };
 
   # ============================================================
-  # Starship
+  # Starship Theme Colors
   # ============================================================
 
-  programs.starship.settings.palette = "gruvbox_dark";
+  programs.starship.settings = {
+    palette = "gruvbox_dark";
+
+    palettes.gruvbox_dark = {
+      color_fg0 = "#fbf1c7";
+      color_bg1 = "#3c3836";
+      color_bg3 = "#665c54";
+
+      color_red = "#cc241d";
+      color_green = "#98971a";
+      color_yellow = "#d79921";
+      color_blue = "#458588";
+      color_purple = "#b16286";
+      color_aqua = "#689d6a";
+      color_orange = "#d65d0e";
+    };
+  };
 
   # ============================================================
   # Fuzzel
