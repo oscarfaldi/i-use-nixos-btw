0cfef6c22de22b6387d5ee1e91478986ff54f808
Oscar Faldi <oscarfaldi@gmail.com>
2026-09-21 14:08:06 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -129,6 +129,17 @@
   #   enableSSHSupport = true;
   # };
 
+  # SSH client configuration
+  programs.ssh = {
+    enable = true;
+
+    matchBlocks.cascade = {
+      hostname = "10.10.10.10";
+      user = "root";
+      identityFile = "~/.ssh/id_ed25519_cascade";
+    };
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
